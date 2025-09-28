680ae813bb5a3033f9e0deda1b63749437aa0f17
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-28 13:17:34 +0530

    first sample usage of usereduce

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
+
+type State = {
+	count: number;
+};
+type Action = { type: "increment" } | { type: "decrement" } | { type: "reset" };
 
 function App() {
-  const [count, setCount] = useState(0)
+	const initialState: State = { count: 0 };
+	const reducer = (state: State, action: Action) => {
+		switch (action.type) {
+			case "increment":
+				return { count: state.count + 1 };
+			case "decrement":
+				return { count: state.count - 1 };
+			default:
+				return state;
+		}
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<form>
+			<h1>Learn useReducer</h1>
+			<h2>Counter: {state.count}</h2>
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					dispatch({ type: "increment" });
+				}}>
+				Increment
+			</button>
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					dispatch({ type: "decrement" });
+				}}>
+				Decrement
+			</button>
+		</form>
+	);
 }
 
-export default App
+export default App;
